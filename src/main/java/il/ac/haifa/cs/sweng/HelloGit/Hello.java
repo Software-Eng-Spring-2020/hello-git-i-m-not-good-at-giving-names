5d23b939d11eff43ae2811da95eeb9f19ee0b136
Ahmed <ahmed.jabareen1@gmail.com>
2020-04-21 00:33:46 +0300

    New class been added

---
+++ src/main/java/il/ac/haifa/cs/sweng/HelloGit/Hello.java
@@ -0,0 +1,9 @@
+package il.ac.haifa.cs.sweng.HelloGit;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("This is a new branch");
+		System.out.println("Branch Number 3");
+	}
+}
